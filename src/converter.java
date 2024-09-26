8be9d82baf1b9ba67a1650f33c714637b8d45de3
Chewbacca <higgimat000@warren.k12.in.us>
2024-09-26 12:56:05 -0400

    initial commit

--- src/converter.java
+++ src/converter.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class converter {
+import java.time.LocalDate;
+
+public class converter {
+
+    public static void main(String[] args) {
+
+        LocalDate rightNow = new LocalDate(2024, 9, 26);
+
+    }
+
 }
